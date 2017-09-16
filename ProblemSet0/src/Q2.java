4b1bc465002463b0fbdf5593263f486d9ce100da
JohnB <John.Brosnan@ittralee.ie>
2017-09-16 15:16:29 +0100

    Added loop to Q2

--- ProblemSet0/src/Q2.java
+++ ProblemSet0/src/Q2.java
@@ -1,5 +1,13 @@
 public class Q2 {
     public static void main(String[] args) {
+        int a = 10;
         System.out.println("hi there 3");
+        a = 9;
+        a = 8;
+
+        while(a<10)
+        {
+            a++;
+        }
     }
 }
