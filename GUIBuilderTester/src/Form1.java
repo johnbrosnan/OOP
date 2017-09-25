58406a3b20a56507386bc6d56497f0686e2a795b
JohnB <John.Brosnan@ittralee.ie>
2017-09-25 23:01:00 +0100

    Changed output message

--- GUIBuilderTester/src/Form1.java
+++ GUIBuilderTester/src/Form1.java
@@ -1,2 +1,20 @@
+import javax.swing.*;
+
 public class Form1 {
+
+    private JTextField textField1;
+    private JPanel panel1;
+    private JCheckBox checkBox1;
+    private JRadioButton radioButton1;
+    private JTextArea textArea1;
+    private JComboBox comboBox1;
+    private JTextPane textPane1;
+
+    public JPanel getPanel1() {
+        return panel1;
+    }
+
+    public void setPanel1(JPanel panel1) {
+        this.panel1 = panel1;
+    }
 }
